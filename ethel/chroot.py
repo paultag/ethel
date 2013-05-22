e3f16e5bca45d0342b2932d010fd44294d1307fb
Paul Tagliamonte <tag@pault.ag>
2013-05-22 13:14:25 -0400

    adding abort script

--- ethel/chroot.py
+++ ethel/chroot.py
@@ -6,11 +6,20 @@ import sys
 import os
 
 
-def get_mount_point(session):
+def get_session_file(session):
+    return '/var/lib/schroot/session/%s' % (session)
+
+
+def get_session(session):
     cfg = configparser.ConfigParser()
-    fil = '/var/lib/schroot/session/%s' % (session)
+    fil = get_session_file(session)
     if cfg.read(fil) == []:
         raise KeyError("No such session: `%s' - %s" % (session, fil))
+    return cfg
+
+
+def get_mount_point(session):
+    cfg = get_session(session)
     obj = cfg[session]
     return obj['mount-location']
 
@@ -48,14 +57,23 @@ def scmd(session, command, expected=0, user=None):
 
 
 @contextlib.contextmanager
-def schroot(chroot):
+def schroot(chroot, source=False):
     session = "ethel-%s" % (os.getpid())
-    out, err = safe_run(['schroot', '-b', '-n', session, '-c', chroot])
+    chroot_name = chroot
+    os.environ['CHROOT_FILE_REPACK'] = "false"
+    if source:
+        os.environ['CHROOT_FILE_REPACK'] = "true"
+        chroot_name = "source:%s" % (chroot_name)
+
+    out, err = safe_run(['schroot', '-b', '-n', session, '-c', chroot_name])
 
     try:
         session = out.strip()
         print("[ethel] Started session: %s" % (session))
         yield session
+    except Exception:
+        os.environ['CHROOT_FILE_REPACK'] = "false"
+        raise
     finally:
         out, err = safe_run(['schroot', '-e', '-c', session])
 
@@ -64,3 +82,13 @@ def copy(session, source, dest):
     root = get_mount_point(session)
     dest = os.path.join(root, dest)
     return shutil.copy2(source, dest)
+
+
+def update(chroot):
+    with schroot(chroot, source=True) as session:
+        scmd(session, ['apt-get', 'update'], user='root')
+        scmd(session, ['apt-get', '-y', 'dist-upgrade'], user='root')
+
+
+def run_update():
+    update(*sys.argv[1:])
