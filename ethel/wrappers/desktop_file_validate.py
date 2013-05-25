5e9eac85e8423e8c9c1fad75cdccde336ef7944d
Paul Tagliamonte <tag@pault.ag>
2013-05-24 23:00:48 -0400

    style

--- ethel/wrappers/desktop_file_validate.py
+++ ethel/wrappers/desktop_file_validate.py
@@ -2,21 +2,15 @@ from firehose.model import Issue, Message, File, Location, Point
 import re
 
 
-# screensaver.desktop: error: value "AWESOME" for string
-LINE_EXPR = re.compile(
-    r"(?P<path>.*): (?P<severity>.*): (?P<msg>.*)"
-)
+LINE_EXPR = re.compile(r"(?P<path>.*): (?P<severity>.*): (?P<msg>.*)")
 
 
 def parse_desktop_file_validate(lines):
     for line in lines:
-
         info = LINE_EXPR.match(line).groupdict()
-
         path = info['path']
         message = info['msg']
         severity = info['severity']
-
         yield Issue(cwe=None,
                     testid=None,
                     location=Location(
