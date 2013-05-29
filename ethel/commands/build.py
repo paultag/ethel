a6aae5440b5b98d447b392261171692e57a993dc
Paul Tagliamonte <tag@pault.ag>
2013-05-28 23:22:19 -0400

    meh

--- ethel/commands/build.py
+++ ethel/commands/build.py
@@ -8,5 +8,5 @@ def run(dsc, package, job):
     ftbfs, out, info = sbuild(dsc, suite, arch)
     print(ftbfs, info)
     # fluxbox_1.3.5-1_amd64.changes
-    job = "{package}_{version}_{arch}.changes"
+    changes = "{package}_{version}_{arch}.changes"
     upload(changes, job)
