ff9ca69eb56c84a515dfd91ae7f91d591dd75f49
Paul Tagliamonte <tag@pault.ag>
2013-06-01 22:37:32 -0400

    need to refactor this soon.

--- ethel/commands/adequate.py
+++ ethel/commands/adequate.py
@@ -1,9 +1,18 @@
 from ethel.runners.adequate import adequate
+from ethel.config import load
 
 
 def run(debs, package, job, firehose):
     if any((not x.endswith(".deb") for x in debs)):
         raise Exception("Non-deb given")
 
-    chroot_name = "{suite}-{arch}".format(**package)
+    config = load()
+    all_arch = config['all-arch']
+    if package['arch'] == 'all':
+        arch = all_arch
+
+    chroot_name = "{suite}-{arch}".format(
+        suite=package['suite'],
+        arch=arch
+    )
     return adequate(chroot_name, debs, firehose)
--- ethel/commands/piuparts.py
+++ ethel/commands/piuparts.py
@@ -1,9 +1,19 @@
 from ethel.runners.piuparts import piuparts
+from ethel.config import load
 
 
 def run(debs, package, job, firehose):
     if any((not x.endswith(".deb") for x in debs)):
         raise Exception("Non-deb given")
 
-    chroot_name = "{suite}-{arch}".format(**package)
+    config = load()
+    all_arch = config['all-arch']
+    if package['arch'] == 'all':
+        arch = all_arch
+
+    chroot_name = "{suite}-{arch}".format(
+        suite=package['suite'],
+        arch=arch
+    )
+
     return piuparts(chroot_name, debs, firehose)
