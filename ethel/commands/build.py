728b91b225e1546e842b35bc3cc8e6c5cd75aa59
Paul Tagliamonte <tag@pault.ag>
2013-06-02 13:08:16 -0400

    feh

--- ethel/commands/build.py
+++ ethel/commands/build.py
@@ -1,5 +1,6 @@
 from ethel.runners.sbuild import sbuild
 from ethel.utils import upload
+import glob
 import os
 
 
@@ -10,15 +11,20 @@ def run(dsc, package, job, firehose):
 
     info, out, ftbfs = sbuild(dsc, suite, arch)
 
-    changes = "{source}_{version}_{arch}.changes".format(
+    changes = "{source}*{arch}.changes".format(
         source=package['source'],
-        version=package['version'],
-        arch=arch)
+        arch=arch
+    )
+
+    changes = list(glob.glob(changes))
+
+    if changes == [] and not ftbfs:
+        print(out)
+        raise Exception("Um. No changes but no FTBFS.")
+
+    changes = changes[0]
 
     if os.path.exists(changes):
         upload(changes, job['_id'])
-    elif not ftbfs:
-        print(out)
-        raise Exception("Um. No changes but no FTBFS.")
 
     return (info, out, ftbfs)
